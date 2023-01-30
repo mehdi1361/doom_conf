13a195df7e74ca0255b2a8459745732e75ec7b4f
mehdi <13610522>
2023-01-30 12:20:40 +0330

    first commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ruby-tools poetry saveplace-pdf-view pdf-view-restore pdf-tools nov bookmark-in-project bongo auto-virtualenv ob-go)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
